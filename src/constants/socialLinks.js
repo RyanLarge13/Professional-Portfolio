74b23078a401c8252997ac73b38921fb060a65ee
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-04-16 01:52:19 -0700

    LinkedIn Social Media
    How could I not possibly provide a linked in
    address to my portfolio?
    
    Issue resolved

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,7 +1,11 @@
-import { BsFacebook } from "react-icons/bs";
+import { BsFacebook, BsLinkedin } from "react-icons/bs";
 import { AiFillTwitterCircle, AiFillGithub } from "react-icons/ai";
 
 export default [
+  {
+    icon: BsLinkedin,
+    href: "https://linkedin.com/in/ryan-large",
+  },
   {
     icon: BsFacebook,
     href: "https://m.facebook.com/ryan.large.984?eav=AfZGHI_LbLygPGY_uKdIQsmUnKdo38V1UhHHCcvj1yA71B2ryRPAIX6E03_ikKG0lbU&paipv=0",
